adb2989851d0ddcf30f3e1e2b30230ba1da74c77
Vitalii Savenko <vitaly.savenko12@gmail.com>
2023-09-25 20:42:51 +0300

    after_Modul_11

--- my_game/game_map.py
+++ my_game/game_map.py
@@ -2,6 +2,84 @@ from helpers import log_message
 
 # Поки що не можна створити.
 # Треба змінити архітектуру програми 
+class Cell:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.content = ' '
+
+    def __repr__(self):
+        return f'Cell: {self.x}, {self.y} |{self.content}|'
+    
+    def __str__(self):
+        return f'Cell: {self.x}, {self.y} |{self.content}|'
+
+
+class Map:
+
+    def __init__(self):
+        self.__n = 5
+        self.__m = 5
+        self.map = [[Cell(j, i) for i in range(self.__n)] for j in range(self.__m)]
+        self.__current_x = 0
+        self.__current_y = 0
+
+    def __next__(self):
+
+
+        if self.__current_x == self.__m:
+            self.__current_x = 0
+            self.__current_y += 1
+
+            if self.__current_y == self.__n:
+                raise StopIteration
+
+
+        self.cell = self.map[self.__current_x][self.__current_y]
+        self.cell.content = 'X'
+        self.__current_x += 1
+        
+        return self.cell
+
+    def __iter__(self):
+        self.__current_x = 0
+        self.__current_y = 0
+        return self
+    
+    def __str__(self):
+        self.my_map = ''
+        self.row = '|'
+        for cell in world_map:
+            if self.__current_x < self.__m:
+                self.row += cell.content + '|'
+            if self.__current_x == self.__m - 1:
+                self.my_map += self.row + '\n'
+                self.row = '|'
+
+        return self.my_map
+    
+
+def generate(obj):
+    for i in obj:
+        yield i
+
+        
+
+world_map = Map()
+for cell in world_map:
+    print(cell)
+
+print(world_map)
+
+generator = generate(world_map)
+
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+
+
 def turns(func):
 
     def inner(*qrgs, **kwargs):
